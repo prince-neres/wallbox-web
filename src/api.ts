39c728ea55b10f8328db854c4baa1d451b78351e
Prince Neres <prince84224960@gmail.com>
2023-05-03 18:58:15 -0300

    fix: Interceptor para desogar usuário quando token expirar

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,23 @@
 import axios from "axios";
+import { logout } from "./redux/reducers/userSlice";
+import store from "./redux/store";
 
 const api = axios.create({
   baseURL: "http://localhost:5000/api/",
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    // Se o erro for um token expirado, deslogue o usuário
+    if (
+      error.response?.status === 401 &&
+      error.response?.data?.msg === "Token has expired"
+    ) {
+      store.dispatch(logout());
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
