1891edb9bb7d089084586585f1ead3682a5f299e
Prince Neres <prince84224960@gmail.com>
2023-05-04 00:42:12 -0300

    fix: Adicionando token em todas requisições

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,27 @@
 import axios from "axios";
 import { logout } from "./redux/reducers/userSlice";
 import store from "./redux/store";
+import { toast } from "react-toastify";
 
 const api = axios.create({
   baseURL: "http://localhost:5000/api/",
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const userInfoString = localStorage.getItem("userInfo");
+    if (userInfoString) {
+      const { userInfo } = JSON.parse(userInfoString);
+      if (userInfo?.token) {
+        config.headers.Authorization = `Bearer ${userInfo.token}`;
+      }
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 api.interceptors.response.use(
   (response) => response,
@@ -15,6 +32,7 @@ api.interceptors.response.use(
       error.response?.data?.msg === "Token has expired"
     ) {
       store.dispatch(logout());
+      toast.error("Token espirado, autenticação necessária!");
     }
     return Promise.reject(error);
   }
