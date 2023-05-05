b1c09ddff7304ec0ae8af4c5e34dc2879db4cf08
Prince Neres <prince84224960@gmail.com>
2023-05-05 15:13:02 -0300

    feat: Adicionando url da api em .env

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,7 +4,7 @@ import store from "../redux/store";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/",
+  baseURL: import.meta.env.VITE_BASE_URL,
 });
 
 api.interceptors.request.use(
